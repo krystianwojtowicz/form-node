c5996b21684b3b1bafa0e412b61a8673865d835f
Krystian <krystian1wojtowicz@gmail.com>
2023-10-08 18:59:45 +0200

    removing comments

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -27,35 +27,3 @@ module.exports = {
     "react/jsx-uses-react": "off",
   },
 };
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "es2021": true
-//     },
-//     "extends": [
-//         "eslint:recommended",
-//         "plugin:react/recommended"
-//     ],
-//     "overrides": [
-//         {
-//             "env": {
-//                 "node": true
-//             },
-//             "files": [
-//                 ".eslintrc.{js,cjs}"
-//             ],
-//             "parserOptions": {
-//                 "sourceType": "script"
-//             }
-//         }
-//     ],
-//     "parserOptions": {
-//         "ecmaVersion": "latest",
-//         "sourceType": "module"
-//     },
-//     "plugins": [
-//         "react"
-//     ],
-//     "rules": {
-//     }
-// }
